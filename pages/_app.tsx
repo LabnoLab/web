87853d1ddb1243f3c69ea12e035a96fefb62b617
Sol Igarzabal <hola.labnolab@gmail.com>
2025-07-07 18:01:54 +0200

    Primera versión de la web Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import { Montserrat } from "next/font/google";
 import type { AppProps } from "next/app";
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700", "900"],
+  variable: "--font-montserrat"
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
